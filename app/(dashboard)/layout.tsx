e2a9ed39c1bcaed61f86bf3cc46301ad9422c720
Chris-young42 <3101940893@qq.com>
2025-06-21 00:34:00 +0800

    fix2

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ function layout({ children }: { children: React.ReactNode }) {
 
     return (
         <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
-            <nav className='flex justify-between border-b border-border h-[60px] px-4 py-2'>
+            <nav className='flex justify-between border-b border-border h-[60px] px-4 py-2 items-center'>
                 <Logo/>
                 <div className='flex gap-4 items-center'>
                 <ThemeSwitcher/>
--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -2,7 +2,8 @@
 import { useTheme } from 'next-themes'
 import React, { useEffect, useState } from 'react'
 import { Tabs, TabsList, TabsTrigger } from './ui/tabs'
-import { SunIcon } from 'lucide-react'
+import { Airplay, MoonIcon, SunIcon } from 'lucide-react'
+
 export const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme()
     const [mounted, setmouted] = useState(false)
@@ -15,8 +16,14 @@ export const ThemeSwitcher = () => {
     return (
         <Tabs defaultValue={theme}>
             <TabsList className='border'>
-                <TabsTrigger value='light' onClick={()=>setTheme('light')}>
-                    <SunIcon className='h-[1.2rem] w-[1.2rem]'/>
+                <TabsTrigger value='light' onClick={() => setTheme('light')}>
+                    <SunIcon className='h-[1.2rem] w-[1.2rem]' />
+                </TabsTrigger>
+                <TabsTrigger value='dark' onClick={() => setTheme('dark')}>
+                    <MoonIcon className='h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0' />
+                </TabsTrigger>
+                <TabsTrigger value='system' onClick={() => setTheme('system')}>
+                    <Airplay className='h-[1.2rem] w-[1.2rem]' />
                 </TabsTrigger>
             </TabsList>
         </Tabs>
